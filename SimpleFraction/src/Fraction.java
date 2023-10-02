002ce01b435c4afc53dd310a0dde970ded96609d
Norras <arkusreturn@gmail.com>
2023-10-02 17:14:34 +0200

    Constructors added

--- SimpleFraction/src/Fraction.java
+++ SimpleFraction/src/Fraction.java
@@ -1,7 +1,25 @@
 
 public class Fraction {
+	
+	private int num;
+	private int den;
+	
+	public Fraction(int num,int den) {
+		this.num=num;
+		this.den=den;
+	}
+	
+	public Fraction(int num) {
+		this.num=num;
+		den=1;
+	}
+	
+	public Fraction() {
+		num=0;
+		den=1;
+	}
 
 	public String toString() {
-		return "Je suis une fraction.";
+		return "Je suis la fraction "+num+"/"+den;
 	}
 }
--- SimpleFraction/src/Main.java
+++ SimpleFraction/src/Main.java
@@ -6,7 +6,16 @@ public class Main {
 		System.out.println("SimpleFraction !");
 		
 		Fraction f=new Fraction();
+		assert f.toString() == "Je suis la fraction 0/1";
 		System.out.println(f.toString());
+		
+		Fraction f1=new Fraction(5);
+		assert f1.toString() == "Je suis la fraction 5/1";
+		System.out.println(f1.toString());
+		
+		Fraction f2=new Fraction(4,7);
+		assert f2.toString() == "Je suis la fraction 4/7";
+		System.out.println(f2.toString());
 	}
 
 }
